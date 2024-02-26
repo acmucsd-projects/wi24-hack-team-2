5af5e370cc7a489c1c43c5e7b156ed58e06a3634
Sarah Li <100035287+sli051357@users.noreply.github.com>
2024-02-25 16:02:09 -0800

    for pansy <3

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,28 @@
+import './sign-in.css';
+
+
+export default function SignIn() {
+    return (
+        <main>
+            <body>
+        <div>
+            <h1>sign up</h1>
+        </div>
+
+        <div>
+            <div>
+                <input type="email" class="input-field" placeholder="email"/>
+            </div>
+
+            <div>
+                <input type="password" class="input-field" placeholder="password"/>
+            </div>
+
+            <div>
+                <input type="button" class="input-button" value="next"/>
+            </div>
+        </div>
+    </body>
+        </main>
+    )
+}
---
+++ src/pages/SignIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import SignInPage from '../components/sign-in/sign-in';
+
+const SignIn = () => {
+    return (
+        <>
+            <SignInPage />
+        </>
+    )
+}
+
+export default SignIn;
