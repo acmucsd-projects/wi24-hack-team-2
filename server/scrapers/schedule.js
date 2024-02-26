d8a8707119154592d5c812f5b473271fa14a34ae
Scott Semtner <sjsemtner@gmail.com>
2024-02-25 17:03:00 -0800

    schedule parser progress

--- server/scrapers/schedule.js
+++ server/scrapers/schedule.js
@@ -44,7 +44,7 @@ function parseHTML(html) {
     }
 
     if (
-      row.classList.contains("sectext") ||
+      row.classList.contains("sectxt") ||
       row.classList.contains("nonenrtxt")
     ) {
       const code = `${department} ${course}`;
@@ -53,10 +53,14 @@ function parseHTML(html) {
         courses.set(code, []);
       }
 
+      const section = row.children[3]?.textContent.trim();
+      const date = Date.parse(section);
+
       courses.get(code).push({
         units: units,
         type: row.querySelector("#insTyp")?.textContent.trim(),
-        section: row.children[3]?.textContent.trim(),
+        section: isNaN(date) ? section : undefined,
+        date: isNaN(date) ? undefined : section,
         days: row.children[4]?.textContent.trim(),
         time: row.children[5]?.textContent.trim(),
         location: `${row.children[6]?.textContent?.trim()} ${row.children[7]?.textContent?.trim()}`,
@@ -65,7 +69,56 @@ function parseHTML(html) {
     }
   }
 
-  courses.forEach((v, k) => console.log(k, v.length))
+  courses.forEach((v, k) => console.log(k, v.length));
+
+  const c = courses.get("CHEM 7L");
+  let sections = [];
+  let currentSections = [];
+  let currentFirstLetter;
+  let sharedMeetings = [];
+  for (const row of c) {
+    // if lecture
+    if (row.type == "LE") {
+      console.log("Lecture case");
+      if (currentFirstLetter !== undefined && row.section[0] !== currentFirstLetter) {
+        // console.log(sharedMeetings)
+        // console.log(currentSections)
+        sections.push(
+          ...currentSections.map((section) => ({
+            ...section,
+            meetings: sharedMeetings.concat(section.meetings),
+          }))
+        );
+        currentSections = [];
+        sharedMeetings = [];
+        // add shared meetings to all current sections and move to sections
+      } else {
+        // console.log("something weird happened");
+      }
+
+      sharedMeetings.push(row);
+      // else
+      // something weird happened
+    } else if (
+      row.section?.length === 3 &&
+      row.section.substr(row.section.length - 2) !== "00"
+    ) {
+      // console.log("Section case")
+      // if new section
+      // console.log(row);
+      currentSections.push({ meetings: [], ...row });
+      currentFirstLetter = row.section[0];
+    } else {
+      // console.log("Other case")
+
+      sharedMeetings.push(row);
+    }
+    // if not new section (final, midterm, ...)
+    // add to shared meetings
+    // console.log(row.date);
+  }
+
+  console.log(sections);
 
   // Second pass to actually get the right format
 }
