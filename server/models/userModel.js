1ed814fd87ad999703fdef392f6686567a29694e
Michael He <michaelhe2016@gmail.com>
2024-02-25 16:16:16 -0800

    fix genSalt error

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -34,7 +34,7 @@ userSchema.statics.signup = async function(email, password) {
         throw Error('Email already in use')
     }
 
-    const salt = await bcrypt.getSalt(10)
+    const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({ email, password: hash })
