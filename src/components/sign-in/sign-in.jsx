e3663ecd2ee6aceb55903f7de03d2b1551b69e99
Sarah Li <100035287+sli051357@users.noreply.github.com>
2024-02-29 00:02:05 -0800

    finished sign-in page design (rough)

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -4,25 +4,31 @@ import './sign-in.css';
 export default function SignIn() {
     return (
         <main>
+            <head>
+                <style>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karla"></link>
+                </style>
+            </head>
+
             <body>
-        <div>
-            <h1>sign up now</h1>
-        </div>
+                <div>
+                    <h1>sign up</h1>
+                </div>
 
-        <div>
-            <div>
-                <input type="email" class="input-field" placeholder="email"/>
-            </div>
+                <div>
+                    <div>
+                        <input type="email" class="input-field" placeholder="email"/>
+                    </div>
 
-            <div>
-                <input type="password" class="input-field" placeholder="password"/>
-            </div>
+                    <div>
+                        <input type="password" class="input-field" placeholder="password"/>
+                    </div>
 
-            <div>
-                <input type="button" class="input-button" value="next"/>
-            </div>
-        </div>
-    </body>
+                    <div>
+                        <input type="button" class="input-button" value="next"/>
+                    </div>
+                </div>
+            </body>
         </main>
-    )
+    );
 }
